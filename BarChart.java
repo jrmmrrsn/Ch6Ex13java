bbaccf9561f7a957cc9c457c9cea760d7d4b39dd
Jeremy <Jrmmrrsn@gmail.com>
2016-10-17 21:31:02 -0500

    Add files via upload
    this is an update of the previous code that compiles and runs but still need the method drawChart with loop

---
+++ BarChart.java
@@ -0,0 +1,81 @@
+//the java program that prompts user to enter the scores
+//of five players and prints a bar char consists of stars
+
+import java.util.Scanner;
+
+public class BarChart
+{
+
+	public static void main(String[] args)
+
+	{
+		//create an instance of scanner class
+		Scanner scanner=new Scanner(System.in);
+		//declare integer variables to score for students
+		int artScore=0;
+		int bobScore=0;
+		int calScore=0;
+		int danScore=0;
+		int eliScore=0;
+		int index;  //index variable
+		
+		//prompt for first player
+		System.out.print("Enter points scored by Art>>");
+		artScore=scanner.nextInt();
+
+		System.out.print("Enter points scored by Bob>>");
+		bobScore=scanner.nextInt();
+
+		System.out.print("Enter points scored by Cal>>");
+		calScore=scanner.nextInt();
+	
+		System.out.print("Enter points scored by Dan>>");
+		danScore=scanner.nextInt();
+
+		System.out.print("Enter points scored by Eli>>");
+		eliScore=scanner.nextInt();
+
+		System.out.println("Points for Game\n");
+
+		System.out.print("Art\t");
+
+		for(index=0; index < artScore; index++)
+			System.out.print("*");
+
+			System.out.println();
+
+			System.out.print("Bob\t");
+
+		for(index=0; index < bobScore; index++)
+
+			System.out.print("*");
+
+			System.out.println();
+
+			System.out.print("Cal\t");
+
+		for(index=0; index < calScore; index++)
+
+			System.out.print("*");
+
+			System.out.println();
+
+			System.out.print("Dan\t");
+
+		for(index=0; index < danScore; index++)
+		
+			System.out.print("*");
+
+			System.out.println();
+
+			System.out.print("Eli\t");
+	
+		for(index=0; index < eliScore; index++)
+
+			System.out.print("*");
+
+			System.out.println();
+
+	}
+}
+		
